9c567b465d4a273b5ac3e1c554e655f03bbfc8cb
DeCicco <ldecicco@usgs.gov>
2021-03-12 18:21:10 -0600

    Forgot to push docs

--- tests/testthat/tests_kalman.R
+++ tests/testthat/tests_kalman.R
@@ -1,5 +1,25 @@
 context("Kalman tests")
 
 test_that("Kalman Bootstrapping", {
+  eList <- EGRET::Choptank_eList
+  dailyBootOut <- genDailyBoot(eList, nBoot = 2, jitterOn = TRUE,
+                               nKalman = 2, setSeed = 1)
+  
+  expect_equal(nrow(dailyBootOut), nrow(eList$Daily))
+  expect_equal(ncol(dailyBootOut), 4)
+  
+  expect_equal(round(dailyBootOut[1,1], digits = 2), 126.77)
+  expect_equal(round(dailyBootOut[2,2], digits = 2), 152.92)
+  
+})
+
+
+test_that("Monthly PI", {
+  eList <- EGRET::Choptank_eList
+  dailyBoot <- Choptank_dailyBootOut
+  month_PI <- makeMonthPI(dailyBoot, eList)
+  
+  
+  
   
 })
